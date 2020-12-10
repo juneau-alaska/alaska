6c3b57f95dfb6a50d83ceb850b0729bad26b3c8a
dskae <davidskae@yahoo.com>
2020-12-09 19:05:35 -0800

    implements change password

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -9,58 +9,44 @@ const auth = require('../middleware/auth');
 
 /**
  * @method - PUT
- * @param - /account
- * @description - Update Account
+ * @param - /account/:id/password
+ * @description - Change Password
  */
+router.put("/:id/password", auth, async (req, res) => {
+    const userId = req.params.id
+    const body = req.body;
+    const currentPassword = req.body.currentPassword;
+    const newPassword = req.body.newPassword;
 
-router.put("/", auth, [
-    check("email", "Please enter a valid email").isEmail(),
-    check("userId", "Please enter a userId")
-    .not()
-    .isEmpty()
-  ], async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
-    }
-
-    const { 
-      email, 
-      userId
-    } = req.body;
-    
     try {
-      let acct = await Account.findOne({
-        email
-      });
-      if (!acct) {
-        return res.status(400).json({
-          message: "Account does not exist"
-        });
-      }
-      acct.userId = userId;
+        const acct = await Account.findOne({ "userId": userId });
 
-      await acct.save(function(err) {
-        if (err) {
-          res.status(400).json({
-            message: err.message
+        const isMatch = await bcrypt.compare(currentPassword, acct.password);
+          if (!isMatch) {
+            return res.status(400).json({
+              message: "Current password is incorrect."
+            });
+          }
+
+        const salt = await bcrypt.genSalt(10);
+        var password = await bcrypt.hash(newPassword, salt);
+
+        acct.password = password;
+
+        await acct.save(function(err) {
+            if (err) {
+              res.status(400).json({
+                message: err.message
+              });
+            } else {
+              res.status(200).json({
+                message: "Updated password."
+              });
+            }
           });
-        } else {
-          res.status(200).json({
-            message: "Updated Account"
-          });
-        }
-      });
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({
-        message: "Server Error"
-      });
-    }
-  }
-);
+      } catch (e) {
+        res.send({ message: "Error occurred while changing password." });
+      }
+});
 
 module.exports = router;
